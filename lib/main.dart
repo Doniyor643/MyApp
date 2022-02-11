e6dc83a429c11953e106db9747a75bbbca595d1b
Doniyor643 <doniyorotashov92@gmail.com>
2022-02-11 18:06:05 +0500

    task

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 void main(){
   runApp(const MyApp());
@@ -11,190 +12,125 @@ class MyApp extends StatefulWidget {
 }
 class _MyAppState extends State<MyApp>{
 
+  final List<String> list = [
+    'assets/images/dew.png',
+    'assets/images/dew2.jpg',
+    'assets/images/dew3.jpg',
+    'assets/images/deweloper1.jpg',
+    'assets/images/deweloper2.jpg',
+    'assets/images/notebook1.jpg',
+    'assets/images/notebook2.jpg',
+    'assets/images/notebook3.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.deepOrange,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+          title: const Text('Shopping'),
+          leading: const Icon(Icons.menu),
+          actions: [
+            Padding(
+                padding: const EdgeInsets.all(8),
+                child: Container(
+                  height: 30,
+                  width: 36,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.yellow.shade800
+                  ),
+                  child: Center(child: Text(list.length.toString()),),
+
+                ),)
+          ],
+        ),
         body: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                colors: [
-              Colors.grey.shade900,
-              Colors.grey.shade500,
-              Colors.grey.shade400,
-            ])
-          ),
+          color: Colors.deepOrange,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              const SizedBox(height: 80,),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: const [
-                    Text("Sign Up",style: TextStyle(color: Colors.white,fontSize: 40),),
-                    Text("Welcome",style: TextStyle(color: Colors.white,fontSize: 20),),
-              ],),
-              ),
-              const SizedBox(height: 20,),
-              Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(50),
-                            topRight: Radius.circular(50)
-                        )
-                    ),
-                    child: SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.all(30),
-                        child: Column(children: [
-                          const SizedBox(height: 60,),
-
-                          //email, password
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow: const [
-                                BoxShadow(
-                                  color: Colors.black12,
-                                  blurRadius: 20,
-                                  offset: Offset(0,10),
-                                )
-                              ]
-                            ),
-                            child: Column(children: [
-                              Container(
-                                padding: const EdgeInsets.all(10),
-                                decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey.shade200))),
-                                child: const TextField(
-                                  decoration: InputDecoration(
-                                    hintText: "Fullname",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none,
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                padding: const EdgeInsets.all(10),
-                                decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey.shade200))),
-                                child: const TextField(
-                                  decoration: InputDecoration(
-                                    hintText: "Email",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none,
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                padding: const EdgeInsets.all(10),
-                                decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey.shade200))),
-                                child: const TextField(
-                                  decoration: InputDecoration(
-                                    hintText: "Phone",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none,
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                padding: const EdgeInsets.all(10),
-                                decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey.shade200))),
-                                child: const TextField(
-                                  decoration: InputDecoration(
-                                    hintText: "Password",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none,
-                                  ),
-                                ),
-                              )
-                            ],),
-                          ),
-                          const SizedBox(height: 40,),
-
-                          // Login button
-                          Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 50),
-                            height: 50,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: Colors.green.shade800,
-                            ),
-                            child: const Center(
-                              child:Text("Login", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 30,),
-
-                          // Login with SMS
-                          const Text("Login with SMS",style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),),
-                          const SizedBox(height: 30,),
-
-                          Row(children: [
-                            //Facebook
-                            Expanded(
-                                child: Container(
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                      color: Colors.blue,
-                                      borderRadius: BorderRadius.circular(50)
-                                  ),
-                                  child: const Center(
-                                      child: Text("Facebook",
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold),)
-                                  ),
-                                )
-                            ),
-                            const SizedBox(width: 30,),
-                            //Google
-                            Expanded(
-                                child: Container(
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                      color: Colors.red,
-                                      borderRadius: BorderRadius.circular(50)
-                                  ),
-                                  child: const Center(
-                                      child: Text("Google",
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold),)
-                                  ),
-                                )
-                            ),
-                            const SizedBox(width: 30,),
-                            Expanded(
-                                child: Container(
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                      color: Colors.black,
-                                      borderRadius: BorderRadius.circular(50)
-                                  ),
-                                  child: const Center(
-                                      child: Text("Apple",
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold),)
-                                  ),
-                                )
-                            )
-                          ],)
-
-                        ],
+              Container(
+                margin: const EdgeInsets.all(20),
+                height: 240,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: const DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/deweloper1.jpg')
+                  )
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomRight, 
+                      colors: [
+                        Colors.black.withOpacity(.3),
+                        Colors.black.withOpacity(.3)
+                      ],
+                    )
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text('Programming', style: TextStyle(fontSize: 35, color: Colors.white),),
+                      const SizedBox(height: 20,),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 40),
+                        height: 50,
+                        width: 250,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)
                         ),
+                        child: const Center(child: Text('Buy')),
                       ),
-                    ),
-                  ))
+                      const SizedBox(height: 20,)
 
-          ],),
+                    ],
+                  ),
+                ),
+              ),
+              //body
+              Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 1,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    children: list.map((item) => callOfList(item)).toList(),
+                  )
+              )
+            ],
+          ),
+        ),
+        
+      ),
+    );
+  }
+  Widget callOfList(String item){
+    return Card(
+      color: Colors.transparent,
+      elevation: 0,
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(item)
+            )
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Icon(Icons.favorite_outlined, color: Colors.red, size: 35,)
+          ],
         ),
       ),
     );
